987f23cfb8314d062c9165fc6df2d21e96237100
vrkx <141280206+vrkx@users.noreply.github.com>
2024-12-07 16:10:51 -0400

    Pignite file Born
    Starting file

---
+++ Pignite.bat
@@ -0,0 +1,22 @@
+echo  ____                                    __             
+echo /\  _`\   __                          __/\ \__      
+echo   \ \ \L\ \/\_\    ___      __     ___ /\_\ \ ,_\    __   
+echo \ \ ,__/\/\ \ /' _ `\  /'_ `\ /' _ `\/\ \ \ \/  /'__`\ 
+echo  \ \ \/  \ \ \/\ \/\ \/\ \L\ \/\ \/\ \ \ \ \ \_/\  __/ 
+echo   \ \_\   \ \_\ \_\ \_\ \____ \ \_\ \_\ \_\ \__\ \____\
+echo    \/_/    \/_/\/_/\/_/\/___L\ \/_/\/_/\/_/\/__/\/____/
+echo                          /\____/                       
+echo                          \_/__/                        
+
+echo Pingnite made by @vrkx
+echo.
+echo PING LIST :
+echo.
+COLOR 01
+echo.
+echo  NA-EAST Ping :
+ping ping-nae.ds.on.epicgames.com
+
+
+timeout 999
+pause
