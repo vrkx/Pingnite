97ddfaa9fbbaae7c51819ebecf991d3f68b2d35d
vrkx <141280206+vrkx@users.noreply.github.com>
2024-12-07 17:46:43 -0400

    Added All Fortnite Servers
    From NAE to Middle East

--- Pignite.bat
+++ Pignite.bat
@@ -1,3 +1,5 @@
+@echo off
+
 echo  ____                                    __             
 echo /\  _`\   __                          __/\ \__      
 echo   \ \ \L\ \/\_\    ___      __     ___ /\_\ \ ,_\    __   
@@ -8,15 +10,39 @@ echo    \/_/    \/_/\/_/\/_/\/___L\ \/_/\/_/\/_/\/__/\/____/
 echo                          /\____/                       
 echo                          \_/__/                        
 
+
 echo Pingnite made by @vrkx
 echo.
-echo PING LIST :
+echo Refresh Network
+ipconfig /flushdns
+echo.
+echo  PING LIST :
 echo.
 COLOR 01
 echo.
 echo  NA-EAST Ping :
 ping ping-nae.ds.on.epicgames.com
+echo.
+echo  NA-CENTRAL Ping :
+ping ping-nac.ds.on.epicgames.com
+echo.
+echo  NA-WEST Ping :
+ping ping-naw.ds.on.epicgames.com
+echo.
+echo  EUROPE Ping :
+ping ping-eu.ds.on.epicgames.com
+echo.
+echo  OCEANIA Ping :
+ping ping-oce.ds.on.epicgames.com
+echo.
+echo  BRAZIL Ping :
+ping ping-br.ds.on.epicgames.com
+echo.
+echo ASIA Ping :
+ping ping-asia.ds.on.epicgames.com
+echo.
+echo MIDDLE-EAST Ping :
+ping ping-me.ds.on.epicgames.com
 
 
-timeout 999
 pause
